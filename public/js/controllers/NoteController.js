6bf53765e812dfe40410106627bcb4b2a081ff49
Nick Arthur <narthur@ngpvan.com>
2014-08-13 18:55:55 -0400

    firepad is going places

--- public/js/controllers/NoteController.js
+++ public/js/controllers/NoteController.js
@@ -1,12 +1,66 @@
 var app = angular.module('NoteController', []);
 
 app.controller('NoteController', function($scope) {
-	$scope.school = "";
-	$scope.classIdentifier = "";
-	$scope.lecture = "";
-	$scope.note = "";
+	$scope.school = "Binghamton";
+	$scope.classIdentifier = "CS350";
+	$scope.lecture = "anotherLecture";
+	$scope.authenticated = false;
 	$scope.newSchool = function() {
 
+	};
+	var firepadRef = new Firebase("https://burning-fire-602.firebaseio.com/web/data/");
+	var outerRef = null;
+	var usr = null;
+	var otherNotes = null;
+	var authClient = new FirebaseSimpleLogin(firepadRef, function(error, user) {
+			if (error) 
+				console.log(error);
+			if (user) {
+				console.log("hello, " + user.displayName);
+				firepadRef.child('users').child(user.uid).set({
+					displayName: user.displayName,
+					provider: user.provider,
+					provider_id: user.id
+				});
+				usr = user;
+				$scope.authenticated = true;
+				$scope.$apply();
+				console.log($scope.authenticated);
+				var baseUri = $scope.school + "/" + $scope.classIdentifier + "/" + $scope.lecture;
+				outerRef = new Firebase("https://burning-fire-602.firebaseio.com/web/data/" + baseUri);
+				console.log(outerRef);
+				outerRef.once("value", function(snapshot) {
+					console.log(val);
+					var val = snapshot.val();
+					val.forEach(function(noteSnapshot) {
+
+					});
+				}, function(err) {
+						console.log('outerRef.once error?');
+				});
+				firepadRef = new Firebase("https://burning-fire-602.firebaseio.com/web/data/" + baseUri + "/" + usr.uid);
+				init('usersPad', usr.uid);
+
+			}
+		});
+	$scope.login = function() {
+		authClient.login('google');
 	}
-	
+	function init(cssId, usrId) {		
+		//// Create CodeMirror (with lineWrapping on).
+		// this needs to create rather than get it seems
+		var element = document.createElement('div');
+		element.id = cssId;
+		var codeMirror = CodeMirror(document.getElementById(cssId), { lineWrapping: true });
+		//// Create Firepad (with rich text toolbar and shortcuts enabled).
+		var myNotes = Firepad.fromCodeMirror(firepadRef, codeMirror,
+			{ richTextToolbar: true, richTextShortcuts: true, userId: usrId });
+
+		//// Initialize contents.
+		myNotes.on('ready', function() {
+
+		});
+    }
+    
 });
+
